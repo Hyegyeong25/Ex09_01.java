3d6e6551561770ef5833f29c41b02fc58d06d124
Joohyegyeong <hyegyeong25@gmail.com>
2022-03-16 11:39:29 +0900

    "Ex03 variable"

---
+++ src/main/java/Ex03_01.java
@@ -0,0 +1,8 @@
+public class Ex03_01 {
+    public static void main(String[] args) {
+        System.out.println("100+100");
+        System.out.print("\n");
+        System.out.printf("%d", 100+100);
+        System.out.print("\n");
+    }
+}
