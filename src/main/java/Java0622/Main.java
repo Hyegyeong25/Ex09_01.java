2b9ee7bda28b7df148d3e83c06d96def8cf3acca
Joohyegyeong <hyegyeong25@gmail.com>
2022-06-23 23:17:12 +0900

    22.06.23 커밋

--- src/main/java/Java0622/Main.java
+++ src/main/java/Java0622/Main.java
@@ -27,5 +27,7 @@ public class Main {
     public static void main(String[] args){
         Child a=new Child(25,"REAKWON");
         a.showInfo();
+        MyChild c = new MyChild();
+        c.main();
     }
 }
--- src/main/java/Java0622/MyChild.java
+++ src/main/java/Java0622/MyChild.java
@@ -6,9 +6,7 @@ class MyChild extends MyParent {
     public int cal(int num1, int num2){
         return num1 * num2;
     }
-    public static void main(String[] args) {
-        MyChild c = new MyChild();
-        int result = c.cal(2, 3);
-        System.out.println("2 * 3 = " + result);
+    public void main(){
+        System.out.println("2 * 3 = " + cal(2,3));
     }
 }
